2136e47ba5fea263cc4945c2cfb71bcd392d4406
aberllin <ns.kozluk@gmail.com>
2024-01-21 12:36:09 +0100

    add returnTo after login in

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const User = require('../models/user');
 const catchAsyncWrapper = require('../utils/catchAsyncWrapper');
 const passport = require('passport');
+const { storeReturnTo } = require('../../middleware');
 
 router.get('/register', (req, res) => {
   res.render('auth/register');
@@ -33,13 +34,17 @@ router.get('/login', async (req, res) => {
 
 router.post(
   '/login',
+  storeReturnTo,
   passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login',
   }),
   catchAsyncWrapper(async (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl);
+
+    delete res.locals.returnTo;
   }),
 );
 
