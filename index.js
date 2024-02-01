d272ac9429f1976ac770ba6efb4889051a94cc88
aberllin <ns.kozluk@gmail.com>
2024-02-01 16:17:45 +0100

    use api endpoint

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 
 /** ----------- MONGOOSE CONNECTION ----------- */
-const dbUrl = `mongodb://localhost:27017/yelp-camp`;
+const dbUrl = process.env.API_ENDPOINT || `mongodb://localhost:27017/yelp-camp`;
 mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error'));
